fb0805d24fbf208bfbb89d61881a65be60c8a9c5
unknown <adam.thomas@usu.edu>
2023-01-06 15:56:31 -0700

    Added router

---
+++ src/TestPage.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1 class="testing">Testing</h1>
+</template>
+
+<script>
+
+export default {
+    name: 'TestPage',
+    components: {
+    }
+}
+</script>
+
--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './app.css'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import TestPage from '@/TestPage.vue'
+import {createRouter, createWebHistory} from 'vue-router'
+import About from '@/views/About.vue'
 
-createApp(App).mount('#app')
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {path: '/', name: 'TestPage', component: TestPage},
+        {path: '/about', name: 'About', component: About}
+    ]
+})
+
+createApp(App)
+.use(router)
+.mount('#app')
